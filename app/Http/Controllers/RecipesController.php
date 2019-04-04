fdf50dfdd55f982ee64a00478c269c54750ea9e2
Jdshooper <Jdshooper@gmail.com>
2019-04-04 13:35:31 -0600

    Added Unirest + recepiesFromIngredients

--- app/Http/Controllers/RecipesController.php
+++ app/Http/Controllers/RecipesController.php
@@ -82,4 +82,21 @@ class RecipesController extends Controller
     {
         //
     }
+
+    public function recipesFromIngredients($request){
+      $ingredientsArr = $request['ingredients'];
+      $getSite = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ignorePantry=false&ingredients=";
+      for($i = 0; $i < sizeof($ingredientsArr); $i++){
+        $getSite = $getSite . $ingredientsArr[$i];
+        if(!(($i + 1) == sizeof($ingredientsArr))){
+          $getSite = $getSite . '%2C';
+        }
+      }
+
+      $response = Unirest\Request::get($getSite, 
+        array(
+            "X-RapidAPI-Key" => "Your Spoonacular API Key"
+        )
+      );
+    }
 }
