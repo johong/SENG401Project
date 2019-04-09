3156165b2937f6ee17d139f207e8a1fe4f2bada0
Daniel Heyns <>
2019-04-09 17:19:16 -0600

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{asset('js/search.js')}}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -81,5 +82,4 @@
         </main>
     </div>
 </body>
-<script src="{{asset('js/search.js')}}"></script>
 </html>
--- resources/views/main/search.blade.php
+++ resources/views/main/search.blade.php
@@ -1,16 +1,4 @@
-{{-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('css/search.css')}}">
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <title>Main</title>
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
-</head>
-<body> --}}
+
 @extends('layouts.app')
 @section('content')
 <div class="bg-image" style="background-image:url({{asset('Images/indexBackground.jpg')}})"></div>
@@ -55,32 +43,6 @@
         </div>
 
     </div>
+    {{-- <script src="{{asset('js/search.js')}}"></script> --}}
 @endsection
 
-{{-- </body>
-<script src="{{asset('js/search.js')}}"></script>
-<<<<<<< HEAD
-</html> --}}
-=======
-</html>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script>
-    $(document).ready(function(){
-        var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
-        $(".postbutton").click(function(){
-            $.ajax({
-                /* the route pointing to the post function */
-                url: '/postajax',
-                type: 'POST',
-                /* send the csrf-token and the input to the controller */
-                data: {_token: CSRF_TOKEN, message:$(".getinfo").val()},
-                dataType: 'JSON',
-                /* remind that 'data' is the response of the AjaxController */
-                success: function (data) { 
-                    $(".writeinfo").append(data.msg); 
-                }
-            }); 
-        });
-    });    
-</script>
->>>>>>> 9af4a71e22f7b9f5329d973b8952df185a99be3c
