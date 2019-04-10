9f623914be9bf6852ffc877410b54a003f1e6f12
shivODD98 <shivodedra@hotmail.com>
2019-04-09 23:03:22 -0600

    fridge page dece done

--- resources/views/fridge/userfridge.blade.php
+++ resources/views/fridge/userfridge.blade.php
@@ -9,9 +9,11 @@
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
         <script src="main.js"></script>
     </head>
-        <body>
-
-            <div class ="sections-list"> 
+        <body > 
+        <div class= "side-bar-container">
+        <!-- style="background-image:url({{asset('Images/green.jpg')}})"> -->
+        <!-- <div class="bg-image" ></div> -->
+            <!-- <div class ="sections-list"> 
                 <ul >
                     <li>
                         <h1 id="my-fridge">My Fridge</h1>
@@ -20,57 +22,142 @@
                         <h1 id="my-recipe">My Recipes</h1>
                     </li>
                 </ul>
-            </div>
+            </div> -->
 
+            <!-- <h1>Search for recipe</h1> -->
+        <!-- <div class = "search-ingredients">{{-- <form action="./results.html"> --}}
+            <input id = "field" type="text" placeholder="Search for recipe by ingredients...">
+            <button id="send" class="circle-button" type="submit"><img src="{{asset('Images/searchIcon.png')}}" alt="Search"></button>
+        {{-- </form> --}}
+        </div> -->
             <div class = "section-name">
                 <ul >
                     <li>
-                        <h1 id="section">My Fridge</h1>
+                        <div class = "search-ingredients">{{-- <form action="./results.html"> --}}
+                        <div class="header"><h1 id="section">Shiv's Fridge</h1></div>    
+                        
+                             <input id = "field" type="text" placeholder="Search for ingredients...">
+                             <button id="send" class="circle-button" type="submit"><img src="{{asset('Images/searchIcon.png')}}" alt="Search"></button>
+                                {{-- </form> --}}
+                        </div>
                     </li>
                 </ul>
             </div>
 
-            <div class = "side-bar"> 
-            <div class = "ingredient-list">
-                <div class="ingredient">
-                    <p>
-                        A
-                    </p>
-                    <!-- <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>  -->
-                    <span class="closebtn">
-                        &hearts;
-                    </span> 
+            
+                <div class = "side-bar"> 
+                    <div class = "ingredient-list">
+                        <div class="ingredient">
+                            <p>
+                                A
+                            </p>
+                            <!-- <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>  -->
+                            <span class="closebtn">
+                                &times;
+                            </span> 
+                        </div>
+                        <div class="ingredient">
+                            <p>
+                                B
+                            </p>
+                            <!-- <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>  -->
+                            <span class="closebtn">
+                                &times;
+                            </span> 
+                        </div>
+                        <div class="ingredient">
+                            <p>
+                                C
+                            </p>
+                            <!-- <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>  -->
+                            <span class="closebtn">
+                                &times;
+                            </span> 
+                        </div>
+                    </div>
                 </div>
-                <div class="ingredient">
-                    <p>
-                        B
-                    </p>
-                    <!-- <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>  -->
-                    <span class="closebtn">
-                        &hearts;
-                    </span> 
-                </div>
-                <div class="ingredient">
-                    <p>
-                        C
-                    </p>
-                    <!-- <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>  -->
-                    <span class="closebtn">
-                        &hearts;
-                    </span> 
-                </div>
-            </div>
-            </div>
 
-            <div class = "recipes"> 
+            <div class ="recipes"> 
                 <div class="recipe-list">
-
+                    <h3>Favourite Recipes</h3>
                 </div>
+                    <div class="grid">
+                        <div class="recipe-info">
+                            <img src="https://ohsheglows.com/wp-content/uploads/2017/02/10minuteveganpasta-6481.jpg" alt="pasta">
+                            <div class="favbtn">
+                                <span >
+                                    &hearts;
+                                </span> 
+                            </div>
+                            <div class="recipe-title">
+                                <p class="name">This recipe </p>
+                                <p class="prepTime">Ready in: 69 mins</p>
+                                <p class="servings">Servings: 420</p>
+                            </div>
+                        </div>
+                        <div class="recipe-info">
+                            <img src="https://ohsheglows.com/wp-content/uploads/2017/02/10minuteveganpasta-6481.jpg" alt="pasta">
+                            <div class="favbtn">
+                                <span >
+                                    &hearts;
+                                </span> 
+                            </div>
+                            <div class="recipe-title">
+                                <p class="name">This recipe </p>
+                                <p class="prepTime">Ready in: 69 mins</p>
+                                <p class="servings">Servings: 420</p>
+                            </div>
+                        </div>
+                        <div class="recipe-info">
+                            <img src="https://ohsheglows.com/wp-content/uploads/2017/02/10minuteveganpasta-6481.jpg" alt="pasta">
+                            <div class="favbtn">
+                                <span >
+                                    &hearts;
+                                </span> 
+                            </div>
+                            <div class="recipe-title">
+                                <p class="name">This recipe </p>
+                                <p class="prepTime">Ready in: 69 mins</p>
+                                <p class="servings">Servings: 420</p>
+                            </div>
+                        </div>
+
+                        <div class="recipe-info">
+                            <img src="https://ohsheglows.com/wp-content/uploads/2017/02/10minuteveganpasta-6481.jpg" alt="pasta">
+                            <div class="favbtn">
+                                <span >
+                                    &hearts;
+                                </span> 
+                            </div>
+                            <div class="recipe-title">
+                                <p class="name">This recipe </p>
+                                <p class="prepTime">Ready in: 69 mins</p>
+                                <p class="servings">Servings: 420</p>
+                            </div>
+                        </div>
+                        <div class="recipe-info">
+                            <img src="https://ohsheglows.com/wp-content/uploads/2017/02/10minuteveganpasta-6481.jpg" alt="pasta">
+                            <div class="favbtn">
+                                <span >
+                                    &hearts;
+                                </span> 
+                            </div>
+                            <div class="recipe-title">
+                                <p class="name">This recipe </p>
+                                <p class="prepTime">Ready in: 69 mins</p>
+                                <p class="servings">Servings: 420</p>
+                            </div>
+                        </div>
+                        
+                    </div>
             </div>
 
 
+            </div>
+        </body>
 
-            <div class="pagination">
+        <!-- <footer>
+        <div class="pagination">
                 <a href="#">&laquo;</a>
                 <a class="active" href="#">1</a>
                 <a href="#">2</a>
@@ -80,7 +167,5 @@
                 <a href="#">6</a>
                 <a href="#">&raquo;</a>
             </div>
-            
-
-        </body>
+        </footer> -->
 </html>
